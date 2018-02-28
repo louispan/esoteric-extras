69f3e0ee6a6fb2b7db5fcd8a9baa5d0ee412b96e
Louis Pan <louis@pan.me>
2018-02-28 21:53:28 +1100

    added keepMaybes

---
+++ README.md
@@ -0,0 +1,4 @@
+[![Hackage](https://img.shields.io/hackage/v/esoteric-extras.svg)](https://hackage.haskell.org/package/esoteric-extras)
+[![Build Status](https://secure.travis-ci.org/louispan/esoteric-extras.png?branch=master)](http://travis-ci.org/louispan/esoteric-extras)
+
+Miscellaneous extra functions
---
+++ src/Data/Maybe/Esoteric.hs
@@ -0,0 +1,8 @@
+module Data.Maybe.Esoteric where
+
+-- | Eg. keepMaybes :: [(a, Maybe b)] -> [(a, b)]
+-- Create a [(a, b)] with only the Just values in a list of (a, Maybe b)
+keepMaybes :: (Monoid (f (t b)), Applicative f, Foldable f, Traversable t) => f (t (Maybe b)) -> f (t b)
+keepMaybes = foldMap $ \tmb -> case sequenceA tmb of
+    Nothing -> mempty
+    Just tb -> pure tb
