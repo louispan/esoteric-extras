b49d3cec40f47f06287bb857ab25d1fe6f0e16b5
Louis Pan <louis@pan.me>
2018-03-05 23:08:48 +1100

    Added Applicative helpers

--- src/Data/Foldable/Esoteric.hs
+++ src/Data/Foldable/Esoteric.hs
@@ -2,7 +2,7 @@ module Data.Foldable.Esoteric where
 
 import Data.Foldable
 
--- A safer version of 'Data.Foldable.traverse_' where
+-- A safer version of 'traverse_' where the left type arg must be unit ().
 -- mapping function results in unit ().
 traverse_' :: (Foldable t, Applicative f) => (a -> f ()) -> t a -> f ()
 traverse_' = traverse_
