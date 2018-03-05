ba717011caaca5c4d7965e7e4baf6ad6688a4805
Louis Pan <louis@pan.me>
2018-03-05 20:11:17 +1100

    renaming Kleisli functions

--- src/Control/Arrow/Esoteric.hs
+++ src/Control/Arrow/Esoteric.hs
@@ -2,16 +2,16 @@ module Control.Arrow.Esoteric where
 
 import Control.Arrow
 
-rk :: Kleisli m a1 b1
-    -> (a1 -> m b1)
-rk = runKleisli
+-- runK :: Kleisli m a1 b1
+--     -> (a1 -> m b1)
+-- runK = runKleisli
 
-rk1 :: (Kleisli m a1 b1 -> Kleisli m a2 b2)
+underK1 :: (Kleisli m a1 b1 -> Kleisli m a2 b2)
     -> (a1 -> m b1) -> (a2 -> m b2)
-rk1 f x = runKleisli $ f (Kleisli x)
+underK1 f x = runKleisli $ f (Kleisli x)
 
 
-rk2 :: (Kleisli m a1 b1 -> Kleisli m a2 b2 -> Kleisli m a3 b3)
+underK2 :: (Kleisli m a1 b1 -> Kleisli m a2 b2 -> Kleisli m a3 b3)
     -> (a1 -> m b1) -> (a2 -> m b2) -> (a3 -> m b3)
-rk2 f x y = runKleisli $ f (Kleisli x) (Kleisli y)
+underK2 f x y = runKleisli $ f (Kleisli x) (Kleisli y)
 
