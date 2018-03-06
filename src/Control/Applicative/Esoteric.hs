3109f3511773aadebb53e91329f8e839f81a5e19
Louis Pan <louis@pan.me>
2018-03-06 22:47:35 +1100

    renamed operators

--- src/Control/Applicative/Esoteric.hs
+++ src/Control/Applicative/Esoteric.hs
@@ -3,17 +3,17 @@ module Control.Applicative.Esoteric where
 import Control.Applicative
 
 -- A safer version of '*>' where the left type arg must be unit ().
-(*>!) :: Applicative f => f () -> f b -> f b
-(*>!) = (*>)
-infixl 4 *>! -- like *>
+(!*>) :: Applicative f => f () -> f b -> f b
+(!*>) = (*>)
+infixl 4 !*> -- like *>
 
 ($*>) :: (Applicative m, Applicative f) => m (f a) -> m (f b) -> m (f b)
 ($*>) = liftA2 (*>)
 infixl 4 $*> -- like *>
 
-($*>!) :: (Applicative m, Applicative f) => m (f ()) -> m (f b) -> m (f b)
-($*>!) = liftA2 (*>!)
-infixl 4 $*>! -- like *>
+($!*>) :: (Applicative m, Applicative f) => m (f ()) -> m (f b) -> m (f b)
+($!*>) = liftA2 (!*>)
+infixl 4 $!*> -- like *>
 
 ($<*>) :: (Applicative m, Applicative f) => m (f (a -> b)) -> m (f a) -> m (f b)
 ($<*>) = liftA2 (<*>)
